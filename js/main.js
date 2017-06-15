54a3b1ee5aca2580396837a52c88d62d23747855
Yesenia <yeseniasescarcega@gmail.com>
2017-06-15 10:59:58 -0500

    Se agrega funcionalidad para mostrar las tarjetas de todos los temas.

--- js/main.js
+++ js/main.js
@@ -3,16 +3,14 @@ var api = {
   url: 'http://examen-laboratoria-sprint-5.herokuapp.com/topics/'
 };
 
-var $listaTemas = $("#listaTemas");
+var $listaTemas = $("#listaTemas");//Se obtiene el elemento html que contendrá las tarjetas de los temas
 
-var plantillaTema =  '<ul id="listaTemas" class="collection container">'+
-        '<li class="collection-item avatar center">'+
+var plantillaTema =  '<li data-target="__id__" class="collection-item avatar center">'+
           '<i class="material-icons circle purple-text">perm_identity</i>'+
           '<span class="title cyan-text ">__tituloTema__</span>'+
           '<p><strong>Autor:</strong> __autor__ </p>'+
           '<p><strong>Respuestas:</strong> __numRespuestas__</p>'+
-        '</li>'+
-      '</ul>';
+        '</li>';
 
 var cargarPagina = function () {
   cargarTemas();
@@ -26,9 +24,20 @@ var cargarTemas = function () {
 };
 
 var crearTema = function (tema){
-  var tema = ;
-  var autor = ;
-  var numRespuestas = ;
+  //Se obtienen las propiedades de cada objeto
+  // console.log(tema);
+  var idTema = tema.id;
+  var tituloTema = tema.content;
+  var autor = tema.author_name;
+  var numRespuestas = tema.responses_count;
+  // console.log(tituloTema+ " " + autor + " " + numRespuestas);
+
+  //Se Remplazan valores en la plantillaTarjeta
+  var tarjetaTema = "";
+  tarjetaTema += plantillaTema.replace('__id__',idTema).replace('__tituloTema__',tituloTema).replace('__autor__',autor).replace('__numRespuestas__',numRespuestas);
+  console.log(tarjetaTema);
+  // Se agregan las tarjetas a la listaTemas
+  $listaTemas.append(tarjetaTema);
 };
 
 $(document).ready(cargarPagina);
